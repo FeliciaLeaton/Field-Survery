519f4b244342cf36482b615e22ff082ba0a898c3
Felicia Leaton <felicialeaton@gmail.com>
2020-07-24 19:18:54 -0500

    Initial Commit

---
+++ Field Survery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Field Survery
+//
+//  Created by Emily Bierma on 7/24/20.
+//  Copyright © 2020 Felicia Leaton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
